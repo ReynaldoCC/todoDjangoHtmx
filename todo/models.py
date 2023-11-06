56eb0231a835c410963e05877c3df3c5d5846d51
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-11-06 16:12:42 +0700

    added model Todo adn migrations for the model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=255, default='')
+    completed = models.BooleanField(verbose_name=_('Completed'), default=False, blank=True)
+
+    class Meta:
+        ordering = ('title', 'completed')
+
+    def __str__(self):
+        return self.title
